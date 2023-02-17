ca2b680ad935b85d1c30f35ef17f309f5e5173bb
houfu <houfu@outlook.stg>
2023-02-18 00:37:13 +0800

    Chat UI now shows a dialogue.

--- your_amicus/chat.py
+++ your_amicus/chat.py
@@ -5,65 +5,77 @@ import datetime
 
 import pynecone as pc
 
-from your_amicus.chains import DefaultChain
 from your_amicus.state import State
 
 
-class Message(pc.Base):
+class Message(pc.Model, table=True):
+    user: str
     text: str
-    sent_time: str
-    outgoing: bool
+    sent_time: float = datetime.datetime.now().timestamp()
+    outgoing: str
 
 
 def get_result(prompt: str) -> str:
-    llm = DefaultChain()
-    result = llm.predict(prompt=prompt).strip()
-    return result
+    # llm = DefaultChain()
+    # result = llm.predict(prompt=prompt).strip()
+    import time
+    time.sleep(1.5)
+    return f"I hear you! You said '{prompt}'"
 
 
 class ChatState(State):
     input_message: str
-    messages: list[Message] = []
     is_waiting: bool = False
+    user: int = 1
+    messages: list[Message] = [
+        Message(text="Hello there! This is the Amicus. I am here to help. Let's talk!", user=1, outgoing="Amicus")]
 
-    def handle_new_message(self):
-        self.messages.append(Message(text=self.input_message, outgoing=True, sent_time=str(datetime.datetime.now())))
-        self.messages = self.messages
-        print(self.messages[-1])
-        self.is_waiting = True
-        prompt = self.input_message
+    def toggle_is_waiting(self):
+        self.is_waiting = not self.is_waiting
+
+    def clear_input(self):
         self.input_message = ""
-        self.messages.append(Message(text=get_result(prompt), outgoing=False,
-                                     sent_time=str(datetime.datetime.now())))
-        self.messages = self.messages
-        self.is_waiting = False
 
+    def save_outgoing_message(self):
+        if self.input_message != "":
+            self.messages = self.messages + [Message(text=self.input_message, outgoing="You", user=self.user)]
 
-def render_message(message: Message, index):
-    return pc.cond(
-        message.outgoing,
-        pc.flex(
-            pc.flex(
-                pc.text(message.message),
-                bg="black", color="white", minW="100px", maxW="350px", my="1", p="3"
-            ),
-            key=index, w="100%", justify="flex-end"
-        ),
-        pc.flex(
-            pc.avatar(ame='Your Amicus', src="android-chrome-192x192.png"),
-            pc.flex(
-                pc.text(message.message),
-                bg="black", color="white", minW="100px", maxW="350px", my="1", p="3"
-            ),
-            key=index, w="100%"
-        )
-    )
+    def save_incoming_message(self):
+        if self.input_message != "":
+            self.messages = self.messages + [
+                Message(text=get_result(self.input_message), outgoing="Amicus", user=self.user)]
+
+    def set_input(self, value):
+        self.input_message = value
 
 
 def message_list():
+    def render_message(message: Message, index):
+        amicus_message = pc.list_item(
+            pc.flex(
+                pc.text(message.text),
+                bg="black", color="white", minW="100px", maxW="350px", my="1", p="3"
+            ),
+            key=index, w="100%", justifyContent="flex-start", display="flex"
+        )
+        your_message = pc.list_item(
+            pc.flex(
+                pc.text(message.text),
+                bg="blue", color="white", minW="100px", maxW="350px", my="1", p="3"
+            ),
+            key=index, w="100%", justifyContent="flex-end", display="flex"
+        )
+        return pc.cond(
+            message.outgoing == "Amicus",
+            amicus_message,
+            your_message
+        )
+
     return pc.flex(
-        pc.foreach(
-            ChatState.messages, render_message
+        pc.list(
+            pc.foreach(
+                ChatState.messages, render_message
+            )
         ),
         pc.cond(
             ChatState.is_waiting,
@@ -85,7 +97,13 @@ def message_input():
             "Send",
             bg="black", color="white",
             disabled=ChatState.is_waiting,
-            on_click=ChatState.handle_new_message
+            on_click=[
+                ChatState.save_outgoing_message,
+                ChatState.toggle_is_waiting,
+                ChatState.save_incoming_message,
+                ChatState.clear_input,
+                ChatState.toggle_is_waiting,
+            ]
         ),
         width="100%", margin_top="5px"
     )
@@ -114,7 +132,7 @@ def chat():
             pc.divider(),
             message_input(),
             direction="column",
-            width=["95%", "95%", "75%", "75%", "40%"], height="90%"
+            width="100%", height="100%"
         ),
         width="100%",
         height="90vh",
